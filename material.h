b86b82e7eab4e6c2358282b1cec92b9bcac85600
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-06-09 18:31:32 +0200

    Refraction light formulat proof

--- material.h
+++ material.h
@@ -102,4 +102,30 @@ class metal: public material {
         double fuzz;
 };
 
+class dielectric : public material {
+    public:
+        dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+        bool scatter(const ray& r_in, const hit_record& hit, color& attenuation, ray& scattered)
+        const override {
+            // We make refraction, refract the exact same color of light
+            attenuation = color(1.0, 1.0, 1.0);
+            // If we hit the front of the surface, we raise to -1
+            double ri = hit.front_face ? (1.0/refraction_index) : refraction_index;
+
+            // Compute the unit vector of the incomming ray
+            vec3 unit_direction = unit_vector(r_in.direction());
+            // Compute the refraction
+            vec3 refracted = refract(unit_direction, hit.normal, ri);
+
+            // Scatter the refracted ray
+            scattered = ray(hit.p, refracted);
+
+            return true;
+        }
+
+    private:
+        double refraction_index;
+};
+
 #endif
