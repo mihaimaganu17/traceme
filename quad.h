f29e73c20eb6a4aa01e2c676a4b979f48f77e3c2
mihaimaganu17 <maganu_gx_mihai@yahoo.com>
2024-07-05 10:22:59 +0200

    Added hit implementation

--- quad.h
+++ quad.h
@@ -13,6 +13,12 @@ class quad: public hittable {
         quad(const point3& Q, const vec3& u, const vec3& v, shared_ptr<material> mat)
             : Q(Q), u(u), v(v), mat(mat)
         {
+            // Compute the normal, as the perpendicular vector of the plane made by `u` and `v`
+            auto n = cross(u, v);
+            // Transform it into a unit vector
+            normal = unit_vector(n);
+            // Compute D
+            D = dot(normal, Q);
             set_bounding_box();
         }
 
@@ -27,8 +33,26 @@ class quad: public hittable {
         aabb bounding_box() const override { reuturn bbox; }
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-            // To be implemented
-            return false;
+            // Compute denominator
+            auto denom = dot(normal, r.direction());
+
+            // No hit is the ray is parallel to the plane
+            if (fabs(denom) < 1e-8) {
+                return false;
+            }
+
+            // Return false if the hit point parameter t is outside the ray interval.
+            auto t = (D - dot(normal, r.origin())) / denom;
+            if (!ray_t.contains(t)) return false;
+
+            auto intersection = r.at(t);
+
+            rec.t = t;
+            rec.p = intersection;
+            rec.mat = mat;
+            rec.set_face_normal(r, normal);
+
+            return true;
         }
 
     private:
@@ -40,6 +64,10 @@ class quad: public hittable {
         shared_ptr<material> mat;
         // Bounding box for this current shape
         aabb bbox;
+        // This is just a constant used to compute if a point is contained within the plane
+        double D;
+        // Normal on the surface
+        vec3 normal;
 }
 
 #endif
